9efc0a54a4281442cde5c7ae4fce2f400f2bddc0
Andrew Carlile <andrew.carlil3@gmail.com>
2024-04-05 13:03:22 -0400

    matching frontend to wireframe

--- client/dueling-dinos/src/App.js
+++ client/dueling-dinos/src/App.js
@@ -8,8 +8,41 @@ import BackImage from './images/back.svg';
 import GoImage from './images/go.svg';
 
 const defaultState = atom(
-    {"page": "home"}
+    {"page": "home",
+     "locale": "en",
+     "vote": null,}
 )
+
+const locales = {
+    "locale": {"en": "Espanol",
+               "es": "English"},
+    "heroText": {"en": "HOW DID THEY DIE?",
+                 "es": "¿HOW DID THEY DIE BUT IN SPANISH?"},
+    "homePrompt0": {"en": "You've seen some evidence.",
+                    "es": "You've seen some evidence, but in Spanish."},
+    "homePrompt1": {"en": "How do you interpret it?",
+                    "es": "¿How do you interpret it, but in Spanish?"},
+    "duel": {"en": "Duel",
+             "es": "Duel, but in Spanish"},
+    "dinner": {"en": "Dinner",
+               "es": "Dinner, but in Spanish"},
+    "disaster": {"en": "Disaster",
+                 "es": "Disaster, but in Spanish"},
+    "back": {"en": "Back",
+             "es":"Back"},
+    "vote":{"en":"Vote",
+            "es": "Vote"},
+    "video": {"en": "Video",
+              "es": "Video, but in Spanish"},
+    "duelText":{"en": "The injuries to both dinosaurs may be evidence that they died in battle. But we need to establish exactly when and how the injuries occurred to be sure.",
+                "es": "ESP<The injuries to both dinosaurs may be evidence that they died in battle. But we need to establish exactly when and how the injuries occurred to be sure.>"},
+    "dinnerText":{"en": "This is placeholder Dinner Text.",
+                  "es": "ESP<This is placeholder Dinner Text.>"},
+    "disasterText":{"en": "This is placeholder Disaster Text.",
+                    "es": "ESP<This is placeholder Disaster Text.>"},
+    "voteConfirm":{"en": "If this is how you interpret the evidence, click VOTE below.",
+                   "es": "ESP<If this is how you interpret the evidence, click VOTE below.>"},
+}
 
 const vote_url =  process.env.REACT_APP_SERVER_URL + ":" + process.env.REACT_APP_SERVER_PORT + "/vote";
 
@@ -42,76 +75,118 @@ function ContentPage({state, setState}){
                                       mode: "cors",
                                       headers: {'Content-Type': 'application/json'},
                                       body: JSON.stringify({"vote": vote})});
-        const response = await request;
+
+    }
+    const content = () => {
+        switch (state.vote){
+        case "duel":  return ({...styler(DuelImage), margin: 0});
+        case "dinner": return ({...styler(DinnerImage), margin: 0});
+        case "disaster": return({...styler(DisasterImage), margin: 0})
+        }
     }
     return (<>
-            <h2> {state.page} </h2>
-            <div className='HomePage-Buttons flex-container row' style={{'display':'flex'}}>
+            <div className="Content flex-container" style={{'display':'flex'}}>
             <div className='Prompt-Button'
-            onClick={(event)=>setState({...state, 'page': 'home'})}
-            style={styler(BackImage)}
+            style={content()}
             ></div>
-            <div className='Prompt-Button'
-            onClick={
-                () => {logVote(state.page)
-                       setState({...state, 'page': 'video'})}
-
-            }
-            style={styler(GoImage)}
-            ></div>
+            <div className="Content-text">
+            <p>{locales[state.vote + "Text"][state.locale]}</p>
+            <br/>
+            <p>{locales.voteConfirm[state.locale]}</p>
+            </div>
+            </div>
+            <div className="VotePage-Buttons flex-container" style={{'display':'flex'}}>
+            <div className="VotePage-Button"
+            onClick={(event)=>setState({...state, 'page': 'home', 'vote':null})}>
+            {locales.back[state.locale]}
+            </div>
+            <div className="VotePage-Button"
+            onClick={()=> {logVote(state.vote);
+                           setState({...state, 'page':'video'});
+                          }
+                    }
+            >{locales.vote[state.locale]}
+            </div>
             </div>
             </>);
 }
 
 function HomePage({state, setState}){
     return (<>
+            <div className='HomePage-Prompt flex-container' style={{'display':'flex'}}>
+            <p>{locales.homePrompt0[state.locale]}</p>
+            <br/>
+            <p>{locales.homePrompt1[state.locale]}</p>
+            </div>
             <div className='HomePage-Buttons flex-container row' style={{'display':'flex'}}>
             <div className='Prompt-Button'
             style={styler(DuelImage)}
-            onClick={(event)=>setState({...state, 'page': 'duel'})}
+            onClick={(event)=>setState({...state, 'page': 'content', 'vote':'duel'})}
             ></div>
             <div className='Prompt-Button'
             style={styler(DinnerImage)}
-            onClick={(event)=>setState({...state, 'page': 'dinner'})}
+            onClick={(event)=>setState({...state, 'page': 'content', 'vote':'dinner'})}
             ></div>
             <div className='Prompt-Button'
             style={styler(DisasterImage)}
-            onClick={(event)=>setState({...state, 'page': 'disaster'})}
-            ></div>
-            <div className='Prompt-Button'
-            style={styler(SomeThingElseImage)}
-            onClick={(event)=>setState({...state, 'page': 'somethingElse'})}
+            onClick={(event)=>setState({...state, 'page':'content','vote': 'disaster'})}
             ></div>
             </div>
 
             </>);
 }
 
-function Main (){
-    const [state, setState] = useAtom(defaultState);
+function Main ({state, setState}){
     switch (state.page) {
     case "home":
         return (<HomePage state={state} setState={setState}/>);
     case "video":
         return (<VideoPage state={state} setState={setState}/>);
-    default:
+    case "content":
         return (<ContentPage state={state} setState={setState}/>);
     };
 }
 
-function Header(){
-    return (
+function Header({state, setState}){
+    const headerText = () => {
+        switch(state.page){
+        case 'home': return (locales.heroText[state.locale]);
+        case 'content': return(locales[state.vote][state.locale]);
+        case 'video': return("vote logged!")
+        default: return("video text");
+        }
+    }
+    return (<>
             <header>
-            Welcome to Dueling Dinosaurs
-        </header>
+            <div
+            onClick={(event)=>setState({...state, 'page': 'home', 'vote': null})}
+            > Home </div>
+            <div>
+            {headerText(state.page)}
+            </div>
+            <div
+            onClick={() => {
+                switch(state.locale){
+                case "en": return(
+                    setState({...state, 'locale': 'es'})
+                );
+                case "es": return(
+                    setState({...state, 'locale': 'en'})
+                );
+                }
+            }}
+            >{locales.locale[state.locale]}</div>
+            </header>
+            </>
     )
 }
 
 function App() {
+    const [state, setState] = useAtom(defaultState);
     return (
             <>
-            <Header/>
-            <Main/>
+            <Header state={state} setState={setState}/>
+            <Main state={state} setState={setState}/>
             </>
     );
 }
